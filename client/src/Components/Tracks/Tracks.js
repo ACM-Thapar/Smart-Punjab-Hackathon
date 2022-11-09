08e828f92b74fe9f3267b799121df138061b9c4e
Amit Goyal <agoyal3_be21@thapar.edu>
2022-11-09 11:04:06 +0530

    Created basic structure of rest of the pages

---
+++ client/src/Components/Tracks/Tracks.js
@@ -0,0 +1,16 @@
+import {React, Fragment} from 'react';
+import './Tracks.css';
+
+function Tracks () {
+  return (
+    <Fragment>
+      <section id="tracks">
+        <div className="tracks container-fluid p-5 m-0 row justify-content-center">
+          <h1>Tracks</h1>
+        </div>
+      </section>
+    </Fragment>
+  );
+}
+
+export default Tracks;
