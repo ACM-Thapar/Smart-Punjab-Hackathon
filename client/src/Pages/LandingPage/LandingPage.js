eac61db66f070406f252aa8aab092395a2c80c1a
Amit Goyal <agoyal3_be21@thapar.edu>
2022-11-19 21:45:44 +0530

    Added register button in mobile view

--- client/src/Pages/LandingPage/LandingPage.js
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -8,7 +8,7 @@ import MainBgSmol from './../../Assets/MainBgSmol.png';
 function LandingPage () {
   return (
     <Fragment>
-      <div className="landing container-fluid p-0 m-0">
+      <div className="landing container-fluid p-0 m-0 text-align-center">
         <Fade top>
           <Navbar className="navbar" />
         </Fade>
@@ -19,13 +19,27 @@ function LandingPage () {
         />
         <img src={MainBgSmol} className="d-md-none" alt="BackGround" />
         <Fade bottom>
-          <h1>SMART PUNJAB HACKATHON</h1>
-          <div
+          <div className="text-align-center container-fluid landingContainer">
+            <h1>SMART PUNJAB HACKATHON</h1>
+            <a
+              href="https://smart-punjab-hack.devfolio.co/"
+              style={{
+                textDecoration: 'none',
+                color: 'white',
+                marginTop: '2rem',
+                // marginLeft: 'auto',
+              }}
+              className="custom-cutout-button nav-btn rg-btn d-md-none"
+            >
+              Register
+            </a>
+          </div>
+          {/* <div
             className="apply-button"
             data-hackathon-slug="smart-punjab-hack"
             data-button-theme="dark"
             style={{height: 44, width: 312}}
-          />
+          /> */}
         </Fade>
       </div>
     </Fragment>
