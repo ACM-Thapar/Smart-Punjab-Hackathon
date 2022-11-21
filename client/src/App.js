bb84c1b2c0ccf9b9c32956e0bec0690b4551466e
Amit Goyal <agoyal3_be21@thapar.edu>
2022-11-21 15:44:45 +0530

    updated devfolio sdk

--- client/src/App.js
+++ client/src/App.js
@@ -23,16 +23,16 @@ function App () {
     }, 5000);
   }, []);
 
-  React.useEffect (() => {
-    const script = document.createElement ('script');
-    script.src = 'https://apply.devfolio.co/v2/sdk.js';
-    script.async = true;
-    script.defer = true;
-    document.body.appendChild (script);
-    return () => {
-      document.body.removeChild (script);
-    };
-  }, []);
+  // React.useEffect (() => {
+  //   const script = document.createElement ('script');
+  //   script.src = 'https://apply.devfolio.co/v2/sdk.js';
+  //   script.async = true;
+  //   script.defer = true;
+  //   document.body.appendChild (script);
+  //   return () => {
+  //     document.body.removeChild (script);
+  //   };
+  // }, []);
 
   return (
     <Fragment>
