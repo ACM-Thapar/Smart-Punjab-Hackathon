8a78f7f90c54b2b9a6e97da9163f7bebf5fcbe86
Amit Goyal <agoyal3_be21@thapar.edu>
2022-11-24 00:26:32 +0530

    Problem statements added

--- client/src/Components/Tracks/Tracks.js
+++ client/src/Components/Tracks/Tracks.js
@@ -43,7 +43,7 @@ function Tracks () {
                   onRequestClose={() => setmodal1IsOpen (false)}
                 >
                   <h2>Automation</h2>
-                  {/* <div id="tabstabs" className="tabs">
+                  <div id="tabs" className="tabs">
                     <div id="tabtab" className="tab">
                       <input type="radio" id="rd11" name="rd" />
                       <label
@@ -54,7 +54,9 @@ function Tracks () {
                         Problem Statement 1
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong> Smart Roads:</strong>
+                        {' '}
+                        Smart Pillars or sensors that may use Radio waves to measure the speed of upcoming vehicles and generate centralized data for overspeeding vehicles. Can be used for automatic Challan for violation.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -67,7 +69,9 @@ function Tracks () {
                         Problem Statement 2
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <string>Smart Translator:</string>
+                        {' '}
+                        To create a system for to-translation from one natural language (English) to another (Hindi) or vice versa, It may also have speech-to-text, text-to-speech conversion, classification tasks, designing chatbots, etc.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -80,12 +84,24 @@ function Tracks () {
                         Problem Statement 3
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Smart Test Tool:</strong>
+                        <ul>
+
+                          To build an application with the following features:
+
+                          <li>
+                            Provide a reliable and easy-to-use software tool that allows test creation and test-taking capabilities.
+                          </li>
+                          <li>
+                            Provide a database that stores questions, which can be used to automatically generate tests based on specified criteria
+                          </li>
+                          <li>
+                            Provide a secure test-taking application and powerful automated grading tool and non-automated grading capabilities
+                          </li>
+                        </ul>
                       </div>
                     </div>
-                  </div> */}
-                  {/* <button onClick={() => setmodal1IsOpen (false)}>Close</button> */}
-                  <span>To be Announced</span>
+                  </div>
                 </ModalComponent>
 
                 {/* MadTech */}
@@ -104,7 +120,7 @@ function Tracks () {
                   onRequestClose={() => setmodal2IsOpen (false)}
                 >
                   <h2>Mad-Tech</h2>
-                  {/* <div id="tabstabs" className="tabs">
+                  <div id="tabstabs" className="tabs">
                     <div id="tabtab" className="tab">
                       <input type="radio" id="rd11" name="rd" />
                       <label
@@ -115,7 +131,9 @@ function Tracks () {
                         Problem Statement 1
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Anonymous Ads:</strong>
+                        {' '}
+                        A platform for agencies that stripes away any personal data points but still retains important and relevant information for the client to market to certain audiences
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -128,7 +146,9 @@ function Tracks () {
                         Problem Statement 2
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Shift forecaster:</strong>
+                        {' '}
+                        An application that could give possible outcomes to a business about its campaigns and strategies with the help of big data.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -141,12 +161,12 @@ function Tracks () {
                         Problem Statement 3
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Anti-Ad blocker:</strong>
+                        {' '}
+                        A smart plugin that companies could use in their applications to bypass ad blockers.
                       </div>
                     </div>
                   </div>
-                  <button onClick={() => setmodal2IsOpen (false)}>Close</button> */}
-                  <span>To be Announced</span>
                 </ModalComponent>
               </Bounce>
 
@@ -166,49 +186,9 @@ function Tracks () {
                 onRequestClose={() => setmodal3IsOpen (false)}
               >
                 <h2>Open Innovation</h2>
-                {/* <div id="tabstabs" className="tabs">
-                  <div id="tabtab" className="tab">
-                    <input type="radio" id="rd11" name="rd" />
-                    <label
-                      id="tabtab-label"
-                      className="tab-label"
-                      htmlFor="rd11"
-                    >
-                      Problem Statement 1
-                    </label>
-                    <div id="tabtab-content" className="tab-content">
-                      Coming Soon!
-                    </div>
-                  </div>
-                  <div id="tabtab" className="tab">
-                    <input type="radio" id="rd12" name="rd" />
-                    <label
-                      id="tabtab-label"
-                      className="tab-label"
-                      htmlFor="rd12"
-                    >
-                      Problem Statement 2
-                    </label>
-                    <div id="tabtab-content" className="tab-content">
-                      Coming Soon!
-                    </div>
-                  </div>
-                  <div id="tabtab" className="tab">
-                    <input type="radio" id="rd13" name="rd" />
-                    <label
-                      id="tabtab-label"
-                      className="tab-label"
-                      htmlFor="rd13"
-                    >
-                      Problem Statement 3
-                    </label>
-                    <div id="tabtab-content" className="tab-content">
-                      Coming Soon!
-                    </div>
-                  </div>
-                </div> */}
-                {/* <button onClick={() => setmodal3IsOpen (false)}>Close</button> */}
-                <span>To be Announced</span>
+                {/* <p className="m-5">
+                 
+                </p> */}
               </ModalComponent>
               <Bounce right>
 
@@ -227,7 +207,7 @@ function Tracks () {
                   onRequestClose={() => setmodal4IsOpen (false)}
                 >
                   <h2>Agri-Tech</h2>
-                  {/* <div id="tabstabs" className="tabs">
+                  <div id="tabstabs" className="tabs">
                     <div id="tabtab" className="tab">
                       <input type="radio" id="rd11" name="rd" />
                       <label
@@ -238,7 +218,7 @@ function Tracks () {
                         Problem Statement 1
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        System for providing proper fertilizers, manures, and other nutrients timely as the crop needs on the basis of climate, soil type, etc.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -251,7 +231,9 @@ function Tracks () {
                         Problem Statement 2
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Automated Farming:</strong>
+                        {' '}
+                        Bring automation into the field of farming. Farmers can control farming activities like irrigation, fertilization, Crop monitoring, etc using an android application.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -264,12 +246,22 @@ function Tracks () {
                         Problem Statement 3
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Smart Agro-logistics:</strong>
+                        <ul>
+                          Innovate the supply chain and agriculture logistics in different ways like:
+                        </ul>
+                        <li>
+                          Using blockchain technology for traceability of agri-produce from Farm to Fork
+                        </li>
+                        <li>
+                          Cost-effective sensor-based real-time monitoring & management of storage and transport system
+                        </li>
+                        <li>
+                          IT-based agriculture logistics inclusion custom hiring services
+                        </li>
                       </div>
                     </div>
-                  </div> */}
-                  {/* <button onClick={() => setmodal4IsOpen (false)}>Close</button> */}
-                  <span>To be Announced</span>
+                  </div>
                 </ModalComponent>
                 <div id="item-4" onClick={() => setmodal5IsOpen (true)}>
                   <img
@@ -285,7 +277,7 @@ function Tracks () {
                   onRequestClose={() => setmodal5IsOpen (false)}
                 >
                   <h2>Smart Cities</h2>
-                  {/* <div id="tabstabs" className="tabs">
+                  <div id="tabstabs" className="tabs">
                     <div id="tabtab" className="tab">
                       <input type="radio" id="rd11" name="rd" />
                       <label
@@ -296,7 +288,9 @@ function Tracks () {
                         Problem Statement 1
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Resilient Cities:</strong>
+                        {' '}
+                        To construct applications that can give data with respect to the city if there should be an occurrence of crisis to protect the general population.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -309,7 +303,9 @@ function Tracks () {
                         Problem Statement 2
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong>Innovative Governance:</strong>
+                        {' '}
+                        A platform for Governments to bring better services, Transparency, Accountability, and Data-driven decisions.
                       </div>
                     </div>
                     <div id="tabtab" className="tab">
@@ -322,12 +318,12 @@ function Tracks () {
                         Problem Statement 3
                       </label>
                       <div id="tabtab-content" className="tab-content">
-                        Coming Soon!
+                        <strong> Smart Transportation:</strong>
+                        {' '}
+                        Using digital tools and systems to improve and innovate the urban transportation experience, resolve traffic issues such as traffic jams, and reduce the number of accidents.
                       </div>
                     </div>
-                  </div> */}
-                  {/* <button onClick={() => setmodal5IsOpen (false)}>Close</button> */}
-                  <span>To be Announced</span>
+                  </div>
                 </ModalComponent>
               </Bounce>
             </div>
